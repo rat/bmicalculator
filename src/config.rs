1abb3bd329962f27619c898aafff31d779129cad
Johannes Böhler <github@johannesboehler.at>
2024-10-16 20:16:05 +0200

    Prepare release

--- src/config.rs
+++
@@ -1,4 +0,0 @@
-pub static VERSION: &str = "1.0";
-pub static GETTEXT_PACKAGE: &str = "bmicalculator";
-pub static LOCALEDIR: &str = "/app/share/locale";
-pub static PKGDATADIR: &str = "/app/share/bmicalculator";
